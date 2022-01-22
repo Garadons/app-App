e8a4bbf6f2a88aa457f99c0609dddbc87c21e1e4
Pasha Scherbak <vippashasherbak@gmail.com>
2022-01-22 19:03:52 +0200

    Destructure data for request

--- src/Api/Services/request.js
+++ src/Api/Services/request.js
@@ -5,7 +5,7 @@ async function request(url, method = "GET", data = null) {
 
     if (data) {
       headers["Content-Type"] = "application/json";
-      body = JSON.stringify({ data });
+      body = JSON.stringify({ ...data });
     }
 
     const response = await fetch(url, {
